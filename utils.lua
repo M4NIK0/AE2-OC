9e83b8dbf7822ad9de3bbf41caedd2ad73096121
Lucas Doolaeghe <lulu.doolaeghe@gmail.com>
2024-01-24 22:21:44 +0100

    Make a module for utils.lua

--- utils.lua
+++ utils.lua
@@ -1,10 +1,13 @@
+-- utils.lua
 local filesystem = require("filesystem")
 
-function directoryExists(path)
+local utils = {}
+
+function utils.directoryExists(path)
     return filesystem.exists(path) and filesystem.isDirectory(path)
 end
 
-function randomString(length)
+function utils.randomString(length)
     local res = ""
     for i = 1, length do
         res = res .. string.char(math.random(97, 122))
@@ -12,7 +15,7 @@ function randomString(length)
     return res
 end
 
-function printFile(filename)
+function utils.printFile(filename)
     local file = io.open(filename, "r")
     if file then
         for line in file:lines() do
@@ -24,3 +27,5 @@ function printFile(filename)
         os.exit(84)
     end
 end
+
+return utils
