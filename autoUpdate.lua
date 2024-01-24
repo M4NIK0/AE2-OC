fdce83ddc38916391e0df9d70bb1a4d611348a89
Lucas Doolaeghe <lulu.doolaeghe@gmail.com>
2024-01-24 22:37:07 +0100

    Checkfile after update

--- autoUpdate.lua
+++ autoUpdate.lua
@@ -2,20 +2,30 @@ local utils = require("utils")
 local baseRepo = "https://raw.githubusercontent.com/M4NIK0/AE2-OC/main/"
 local downloadPath = "/home/manager"
 
+function checkDownload(tmpFolder)
+    local file = io.open("/tmp/" .. tmpFolder .. "/dependencies.txt", "r")
+    if file then
+        for line in file:lines() do
+            local dlfile = io.open("/tmp/" .. tmpFolder .. "/" .. line, "r")
+            if file then
+                dlfile:close()
+            else
+                print("ERROR: Downloading " .. line .. " Failed")
+                os.exit(84)
+            end
+        end
+        file:close()
+    else
+        print("Failed to open dependencies.txt")
+    end
+end
+
 function downloadFileFromRepo(filename, tmpFolder)
     local url = baseRepo .. filename
     local tmpPath = "/tmp/" .. tmpFolder .. "/" .. filename
 
     local command = "wget " .. url .. " -O " .. tmpPath
-    local exitCode = os.execute(command)
-
-    if exitCode == 0 then
-        print("Downloaded:", filename)
-    else
-        print("Failed to download:", filename)
-        print("Stopping update.")
-        os.exit(84)
-    end
+    os.execute(command)
 end
 
 function downloadFilesFromRepo(tmpFolder)
@@ -31,9 +41,11 @@ function downloadFilesFromRepo(tmpFolder)
     else
         print("Failed to open dependencies.txt")
     end
+    checkDownload(tmpFolder)
     os.execute("rm -rf " .. downloadPath)
-    os.execute("cp -r " .. tmpFolder .. " " .. downloadPath)
+    os.execute("cp -r /tmp/" .. tmpFolder .. " " .. downloadPath)
     os.execute("rm -rf /tmp/" .. tmpFolder)
 end
+
 
 downloadFilesFromRepo(utils.randomString(16))
